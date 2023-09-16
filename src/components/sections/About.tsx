fd769da2c636c71b54baafb5b8a10ffb3175970a
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2023-09-17 02:03:36 +0300

    update about

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -4,11 +4,18 @@ export default function About() {
 	return (
 		<Section id="about" title="About">
 			<p className="m-auto max-w-3xl">
-				Hi, I&apos;m Ahmed AbouEleyuon, I&apos;m a GIS graduate and a
-				computer science student at the university of Cairo, I&apos;m
-				also a software engineering intern at ALX. I&apos;m very
-				passionate about computer science and software development, and
-				I love cats.
+				Hi, I&apos;m Ahmed AbouEleyuon, a GIS graduate currently
+				pursuing a diploma in computer science at the University of
+				Cairo. I&apos;m also gaining valuable experience as a software
+				engineering intern at ALX.
+				<br />
+				<br />
+				I&apos;m very passionate about computer science and software
+				development, I&apos;m constantly learning new concepts and
+				technologies, and my goal is to make a positive impact on the
+				world.
+				<br />
+				<br />I use Arch Linux (BTW) and Neovim. And I love cats.
 			</p>
 		</Section>
 	);
