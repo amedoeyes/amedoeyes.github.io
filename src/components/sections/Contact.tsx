db8af7edabf66bb5cf8b235df76219ea2609534c
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-03-09 03:27:48 +0200

    add icons to contacts

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -3,20 +3,19 @@ import Section from "../Section";
 export default function Contact() {
 	return (
 		<Section id="contact" title="Contact">
-			<p className="mb-2 text-center">Feel free to contact me</p>
 			<div className="flex flex-col justify-center gap-2 underline sm:flex-row sm:gap-8">
 				<div className="flex flex-col">
 					<a
 						href="mailto:ahmed.m.aboueleyoun@gmail.com"
 						className="duration-200 hover:opacity-75"
 					>
-						ahmed.m.aboueleyoun@gmail.com
+						󰇮 ahmed.m.aboueleyoun@gmail.com
 					</a>
 					<a
 						href="phone:+20 100 240 3588"
 						className="duration-200 hover:opacity-75"
 					>
-						+20 100 240 3588
+						 +20 100 240 3588
 					</a>
 				</div>
 				<div className="flex flex-col">
@@ -24,13 +23,13 @@ export default function Contact() {
 						href="https://github.com/amedoeyes"
 						className="duration-200 hover:opacity-75"
 					>
-						GitHub
+						 GitHub
 					</a>
 					<a
 						href="https://linkedin.com/in/ahmed-aboueleyuon"
 						className="duration-200 hover:opacity-75"
 					>
-						LinkedIn
+						 LinkedIn
 					</a>
 				</div>
 			</div>
