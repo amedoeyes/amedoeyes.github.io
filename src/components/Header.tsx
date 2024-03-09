3cccfef9ef841ec7ee36baabc7aa6f8a9d1e2c50
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-03-09 02:30:39 +0200

    style header to be thinner

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,28 +12,24 @@ export default function Header() {
 		const updateHeader = () => {
 			if (window.scrollY >= hero.clientHeight - header.clientHeight) {
 				header.classList.add("bg-secondary");
-
 				header.classList.remove("border-b-transparent");
 				header.classList.add("border-b-primary");
 			} else {
 				header.classList.remove("bg-secondary");
-
 				header.classList.remove("border-b-primary");
 				header.classList.add("border-b-transparent");
 			}
 		};
-
 		updateHeader();
 
 		window.addEventListener("scroll", updateHeader);
-
 		return () => {
 			window.removeEventListener("scroll", updateHeader);
 		};
 	}, []);
 
 	return (
-		<header className="fixed z-40 flex h-20 w-full items-center justify-between border-b border-b-transparent border-opacity-50 px-6 duration-200">
+		<header className="fixed z-40 flex h-16 w-full items-center justify-between border-b border-b-transparent border-opacity-50 px-6 duration-200">
 			<nav className="flex w-full items-center justify-between text-lg uppercase">
 				<div>
 					<a
