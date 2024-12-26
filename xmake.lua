d34d2027fb1c80e27991cdce2dd31c50fd034b0c
Ahmed AbouEleyoun <amedoeyes@gmail.com>
2024-12-26 03:49:42 +0200

    init

---
+++ xmake.lua
@@ -0,0 +1,8 @@
+target("portfolio", function()
+	set_kind("phony")
+	on_build(function()
+		os.mkdir("build")
+		os.exec(string.format("jinja2 %s %s -o %s", "templates/main.html", "data/data.json", "build/index.html"))
+		os.cp(path.join("static", "*"), "build")
+	end)
+end)
